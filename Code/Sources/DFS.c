3e60aedfe5cc63e7559b671f502253e5ab9355d2
Lăscău Ionut Sebastian <32646125+lascau@users.noreply.github.com>
2018-06-05 23:05:21 +0300

    Update DFS.c

--- Code/Sources/DFS.c
+++ Code/Sources/DFS.c
@@ -7,7 +7,7 @@ void DFS(struct a_graph *graph, int start_node){
     int iterator_nodes;
     int temp_node;
 
-    stack = calloc(graph->no_nodes ,sizeof(int));
+    stack = calloc(graph->no_nodes, sizeof(int));
     no_elements_stack = 1;
     stack[no_elements_stack] = start_node;
     *(graph->is_visited + start_node) = 1;
@@ -15,8 +15,8 @@ void DFS(struct a_graph *graph, int start_node){
     while (no_elements_stack > 0){
         temp_node = stack[no_elements_stack];
         no_elements_stack--;
-        for (iterator_nodes = 1; iterator_nodes <= graph->no_nodes; iterator_nodes++)
-            if (*(graph->is_visited + iterator_nodes) == 0 && *(graph->adj_matrix + (iterator_nodes - 1) * graph->no_nodes + temp_node - 1) == 1){
+        for (iterator_nodes = 0; iterator_nodes < graph->no_nodes; iterator_nodes++)
+            if (*(graph->is_visited + iterator_nodes) == 0 && *(graph->adj_matrix + iterator_nodes * graph->no_nodes + temp_node) == 1){
                 *(graph->is_visited + iterator_nodes) = 1;
                 no_elements_stack++;
                 stack[no_elements_stack] = iterator_nodes;
